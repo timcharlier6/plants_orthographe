3ac4870f073189324352d6f4f0def7fee096b6ed
Tim Charlier <timcharlier6@etik.com>
2024-08-27 18:27:02 +0200

    aeu

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ $(document).ready(function () {
   const $p = $("#p");
   const $textarea = $("#textarea");
   const $start = $("#start");
-  const notes = "qdjaejae␣qdjaejae↵".split("");
-  let input = "";
+  const notes = "qdjaejaeqdjaejae␣".split("");
 
-  // Create spans for each note and append to the paragraph
   notes.forEach((note) => {
     const $span = $("<span>").text(note);
     $p.append($span);
@@ -16,58 +14,34 @@ $(document).ready(function () {
     $textarea.focus();
   });
 
-  let lastKeyCode = null;
-
-  $textarea.on("keydown", function (event) {
-    lastKeyCode = event.which;
-  });
-
   $textarea.on("input", (e) => {
-    let specialChar = "";
-    if (lastKeyCode === 8) {
-      // Backspace key pressed
-      input = input.slice(0, -1); // Remove last character from input
-      $spans.eq(input.length).css("color", ""); // Reset color of the span
-      lastKeyCode = null; // Reset lastKeyCode
-      return; // Exit the function early, no need to process further
-    }
+    let inputText = $textarea.val();
+    let lastInputLength = 0;
 
-    switch (lastKeyCode) {
-      case 32:
-        // Space key pressed
-        specialChar = "␣";
-        break;
-      case 13:
-        // Enter key pressed
-        specialChar = "↵";
-        break;
-      default:
-        specialChar = "";
-        break;
-    }
+    inputText = inputText.replace(/ /g, "␣");
 
-    // Add the new character to input string
-    const char = specialChar === "" ? e.target.value.slice(-1) : specialChar;
-    input += char;
-
-    // Clear textarea value to prevent duplication
-    $textarea.val("");
-
-    // Update span colors based on current input
     $spans.each((index, span) => {
       const $span = $(span);
-      if (index < input.length) {
-        if (input[index] !== $span.text()) {
-          $span.css("color", "red"); // Mark wrong characters
+      if (lastInputLength > inputText.length) {
+        $spans.eq(lastInputLength).removeClass("underline");
+        $span.eq(lastInputLength).css("color", "");
+      }
+      if (index < inputText.length) {
+        $spans.eq(index + 1).addClass("underline");
+        $span.removeClass("underline");
+
+        if (inputText[index] !== $span.text()) {
+          $span.css("color", "red");
         } else {
-          $span.css("color", "green"); // Mark correct characters
+          $span.css("color", "green");
         }
-      } else {
-        $span.css("color", ""); // Reset the color for future inputs
+        lastInputLength = inputText.length;
+        console.log(lastInputLength, inputText.length, $span.text());
       }
     });
-
-    // Clear lastKeyCode to avoid incorrect behavior
-    lastKeyCode = null;
+    if (inputText.length >= $spans.length) {
+      $spans.css("color", "");
+      $textarea.val("");
+    }
   });
 });
