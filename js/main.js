a6539d279e7d974921de400c84be8ef790341ec2
Tim Charlier <timcharlier6@etik.com>
2024-08-27 19:19:42 +0200

    fix space next

--- js/main.js
+++ js/main.js
@@ -2,13 +2,28 @@ $(document).ready(function () {
   const $p = $("#p");
   const $textarea = $("#textarea");
   const $start = $("#start");
-  const notes = "qdjaejaeqdjaejae␣".split("");
+  const notes1 = "qdjaejaeqdjaejae␣".split("");
+  const notes2 = "aejaeqdjaejaeqdj␣".split("");
+  const allNotes = [notes1, notes2];
+  let notes;
+  let $spans;
+  console.log(notes);
+  console.log(allNotes);
 
-  notes.forEach((note) => {
-    const $span = $("<span>").text(note);
-    $p.append($span);
-  });
-  const $spans = $p.children();
+  function nextNotes() {
+    if (allNotes.length === 0) {
+      $p.text("Finish");
+      return;
+    }
+    notes = allNotes.shift();
+    $p.contents().remove();
+    notes.forEach((note) => {
+      const $span = $("<span>").text(note);
+      $p.append($span);
+    });
+    $spans = $p.children();
+  }
+  nextNotes();
 
   $start.on("click", () => {
     $textarea.focus();
@@ -36,9 +51,13 @@ $(document).ready(function () {
         $span.removeClass("underline");
       }
     });
-    if (inputText.length >= $spans.length) {
+    if (
+      inputText.length >= $spans.length &&
+      inputText[inputText.length - 1] === "␣"
+    ) {
       $spans.css("color", "");
       $textarea.val("");
+      nextNotes();
     }
   });
 });
